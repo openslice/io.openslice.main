4d36ea66d4be7daa9850cfcd8f543b4fc09c7103
Christos Tranoris <tranoris@gmail.com>
2020-04-13 00:37:06 +0300

    adding keycloak to compose

---
+++ compose/mysql-init/01-databases.sql
@@ -0,0 +1,11 @@
+# create databases
+CREATE DATABASE IF NOT EXISTS `osdb`; CREATE DATABASE IF NOT EXISTS `keycloak`;
+
+# create root user and grant rights
+CREATE USER 'portaluser'@'localhost' IDENTIFIED BY '12345';
+GRANT ALL PRIVILEGES ON *.* TO 'portaluser'@'%' IDENTIFIED BY '12345';
+
+
+# create root user and grant rights
+CREATE USER 'keycloak'@'localhost' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON *.* TO 'keycloak'@'%' IDENTIFIED BY 'password';
