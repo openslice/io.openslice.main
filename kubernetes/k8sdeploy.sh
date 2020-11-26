a0ec5918d758a31b628524c780b78b8cae49747f
Christos Tranoris <tranoris@gmail.com>
2020-11-26 12:41:26 +0200

    k8s dev

--- kubernetes/k8sdeploy.sh
+++ kubernetes/k8sdeploy.sh
@@ -22,49 +22,49 @@ grep -n 'INGRESSADDR' ./deployment/* | xargs -0 sed 's/INGRESSADDR/$1/g'
 kubectl create namespace openslice
 
 
-kubectl apply -f mysql-portal-claim0-persistentvolumeclaim.yaml
+kubectl apply -f ./deployment/mysql-portal-claim0-persistentvolumeclaim.yaml
 
-kubectl apply -f activemq-deployment.yaml
-kubectl apply -f activemq-service.yaml
+kubectl apply -f ./deployment/activemq-deployment.yaml
+kubectl apply -f ./deployment/activemq-service.yaml
 
-kubectl apply -f consul-deployment.yaml
-kubectl apply -f consul-service.yaml
+kubectl apply -f ./deployment/consul-deployment.yaml
+kubectl apply -f ./deployment/consul-service.yaml
 
-kubectl apply -f mysql-portal-deployment.yaml
-kubectl apply -f mysql-portal-service.yaml
+kubectl apply -f ./deployment/mysql-portal-deployment.yaml
+kubectl apply -f ./deployment/mysql-portal-service.yaml
 
-#kubectl apply -f keycloak-realm-config.yaml
-kubectl create configmap keycloak-realm-config --from-file=realm-export.json --namespace=openslice
-kubectl apply -f keycloak-deployment.yaml
-kubectl apply -f keycloak-service.yaml
+#kubectl apply -f ./deployment/keycloak-realm-config.yaml
+kubectl create configmap keycloak-realm-config --from-file=./deployment/realm-export.json --namespace=openslice
+kubectl apply -f ./deployment/keycloak-deployment.yaml
+kubectl apply -f ./deployment/keycloak-service.yaml
 
-kubectl apply -f osom-claim0-persistentvolumeclaim.yaml
-kubectl apply -f osom-deployment.yaml
-kubectl apply -f osom-service.yaml
+kubectl apply -f ./deployment/osom-claim0-persistentvolumeclaim.yaml
+kubectl apply -f ./deployment/osom-deployment.yaml
+kubectl apply -f ./deployment/osom-service.yaml
 
-kubectl apply -f manoclient-deployment.yaml
-kubectl apply -f manoclient-service.yaml
+kubectl apply -f ./deployment/manoclient-deployment.yaml
+kubectl apply -f ./deployment/manoclient-service.yaml
 
-kubectl apply -f osportalapi-claim0-persistentvolumeclaim.yaml
-kubectl apply -f osportalapi-deployment.yaml
-kubectl apply -f osportalapi-service.yaml
+kubectl apply -f ./deployment/osportalapi-claim0-persistentvolumeclaim.yaml
+kubectl apply -f ./deployment/osportalapi-deployment.yaml
+kubectl apply -f ./deployment/osportalapi-service.yaml
 
-kubectl apply -f osscapi-claim0-persistentvolumeclaim.yaml
-kubectl apply -f osscapi-deployment.yaml
-kubectl apply -f osscapi-service.yaml
+kubectl apply -f ./deployment/osscapi-claim0-persistentvolumeclaim.yaml
+kubectl apply -f ./deployment/osscapi-deployment.yaml
+kubectl apply -f ./deployment/osscapi-service.yaml
 
-kubectl apply -f tmfweb-config.yaml
-kubectl apply -f tmfweb-deployment.yaml
-kubectl apply -f tmfweb-service.yaml
+kubectl apply -f ./deployment/tmfweb-config.yaml
+kubectl apply -f ./deployment/tmfweb-deployment.yaml
+kubectl apply -f ./deployment/tmfweb-service.yaml
 
-kubectl apply -f portalweb-config.yaml
-kubectl apply -f portalweb-deployment.yaml
-kubectl apply -f portalweb-service.yaml
+kubectl apply -f ./deployment/portalweb-config.yaml
+kubectl apply -f ./deployment/portalweb-deployment.yaml
+kubectl apply -f ./deployment/portalweb-service.yaml
 
-kubectl apply -f centrallog-deployment.yaml
-kubectl apply -f centrallog-service.yaml
+kubectl apply -f ./deployment/centrallog-deployment.yaml
+kubectl apply -f ./deployment/centrallog-service.yaml
 
-kubectl apply -f bugzilla-client-deployment.yaml
-kubectl apply -f bugzilla-client-service.yaml
+kubectl apply -f ./deployment/bugzilla-client-deployment.yaml
+kubectl apply -f ./deployment/bugzilla-client-service.yaml
 
 
