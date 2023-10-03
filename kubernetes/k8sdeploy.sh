957909a9c1c1aa8378311ba25f82fc944a1d4d7c
Christos Tranoris <tranoris@gmail.com>
2023-10-03 12:48:23 +0300

    deployment script changes

--- kubernetes/k8sdeploy.sh
+++ kubernetes/k8sdeploy.sh
@@ -4,9 +4,12 @@ CURRENT_DIR="$(pwd)"
 SCRIPT_DIR="$(dirname "$0")"
 cd $SCRIPT_DIR
 
+# copy to a workingdir
+cp -r ./template ./_apply
+
 kubectl create namespace openslice
 
-kubectl apply -f ./template/openslice-ingress.yaml
+kubectl apply -f ./_apply/openslice-ingress.yaml
 
 # wait for ingress to get IP address
 INGRESSADDR=""
@@ -23,44 +26,43 @@ while [ -z $INGRESSADDR ]; do
   [ -z "$INGRESSADDR" ] && sleep 5
 done
 echo 'Found external IP: '$INGRESSADDR
+find ./_apply/ -type f  -print0 |  xargs -0 sed -i "s/INGRESSADDR/$INGRESSADDR/g"
 
-find ./template/ -type f  -print0 |  xargs -0 sed -i "s/INGRESSADDR/$INGRESSADDR/g"
+kubectl apply -f ./_apply/artemis.yaml
+kubectl apply -f ./_apply/bugzilla.yaml
+kubectl apply -f ./_apply/centrallog.yaml
+kubectl apply -f ./_apply/manoclient.yaml
+kubectl apply -f ./_apply/kroki.yaml
+kubectl apply -f ./_apply/blockdiag.yaml
 
-kubectl apply -f ./template/artemis.yaml
-kubectl apply -f ./template/bugzilla.yaml
-kubectl apply -f ./template/centrallog.yaml
-kubectl apply -f ./template/manoclient.yaml
-kubectl apply -f ./template/kroki.yaml
-kubectl apply -f ./template/blockdiag.yaml
+kubectl apply -f ./_apply/portalweb-config.yaml
+kubectl apply -f ./_apply/portalweb-config-nginx.yaml
+kubectl apply -f ./_apply/portalweb.yaml
 
-kubectl apply -f ./template/portalweb-config.yaml
-kubectl apply -f ./template/portalweb-config-nginx.yaml
-kubectl apply -f ./template/portalweb.yaml
+kubectl apply -f ./_apply/tmfweb-config.yaml
+kubectl apply -f ./_apply/tmfweb-config-nginx.yaml
+kubectl apply -f ./_apply/tmfweb.yaml
 
-kubectl apply -f ./template/tmfweb-config.yaml
-kubectl apply -f ./template/tmfweb-config-nginx.yaml
-kubectl apply -f ./template/tmfweb.yaml
+kubectl apply -f ./_apply/mysql-config.yaml
+kubectl apply -f ./_apply/mysql-pv-pvc.yaml
+kubectl apply -f ./_apply/mysql.yaml
 
-kubectl apply -f ./template/mysql-config.yaml
-kubectl apply -f ./template/mysql-pv-pvc.yaml
-kubectl apply -f ./template/mysql.yaml
+kubectl create configmap keycloak-realm-config --from-file=./_apply/realm-export.json --namespace=openslice
+kubectl apply -f ./_apply/keycloak.yaml
 
-kubectl create configmap keycloak-realm-config --from-file=./template/realm-export.json --namespace=openslice
-kubectl apply -f ./template/keycloak.yaml
+kubectl apply -f ./_apply/osom-pv-pvc.yaml
+kubectl apply -f ./_apply/osom.yaml
 
-kubectl apply -f ./template/osom-pv-pvc.yaml
-kubectl apply -f ./template/osom.yaml
+kubectl apply -f ./_apply/oasapi-pv-pvc.yaml
+kubectl apply -f ./_apply/oasapi.yaml
 
-kubectl apply -f ./template/oasapi-pv-pvc.yaml
-kubectl apply -f ./template/oasapi.yaml
+kubectl apply -f ./_apply/osportalapi-pv-pvc.yaml
+kubectl apply -f ./_apply/osportalapi.yaml
 
-kubectl apply -f ./template/osportalapi-pv-pvc.yaml
-kubectl apply -f ./template/osportalapi.yaml
+kubectl apply -f ./_apply/osscapi-pv-pvc.yaml
+kubectl apply -f ./_apply/osscapi.yaml
 
-kubectl apply -f ./template/osscapi-pv-pvc.yaml
-kubectl apply -f ./template/osscapi.yaml
-
-kubectl apply -f ./template/front-networkpolicy.yaml
-kubectl apply -f ./template/back-networkpolicy.yaml
+kubectl apply -f ./_apply/front-networkpolicy.yaml
+kubectl apply -f ./_apply/back-networkpolicy.yaml
 
 cd $CURRENT_DIR
