ac5ea4da318f965c792be2fb8ecf4039e2462759
Dimitris Giannopoulos <30804541+dimitrisgiannopoulos@users.noreply.github.com>
2023-11-02 21:37:41 +0200

    Kubernetes (#22)
    This MR fixes #23 
    
    * fixed relative dir for k8s scripts
    
    * updated all manifests based on compose
    
    * small fix for yamls and script updates
    
    * added web and fixed configs
    
    * completed k8sdeploy, k8sremove scripts
    
    * changes to configuration
    
    * removed unnecessary file
    
    * fixes for running under docker desktop. keycloak issuer is problematic
    
    * fix oasapi probe
    
    * deployment script changes
    
    * removed portainer
    
    * removed network policies
    
    * full update to helm chart
    
    * config fixes to rootURL and INGRESSADDR
    
    * fixes to helm chart
    
    * made pv configs configurable from values
    
    * added README and values configs
    
    * minor fix to autoscaling input
    
    * added serviceName
    
    * added install script
    
    * removed ingress controller
    
    * added checkout to kubernetes branch
    
    * minor fix to rooturl
    
    * added routing for ingress
    
    * made update noninteractive
    
    * chmod +x install
    
    * temp remove needsrestart
    
    * minor fix to repo stage
    
    * repositioned needsrestart
    
    * completely removed needsrestart
    
    * added hostNetwork config
    
    * removed iptable routing
    
    * added hostNetwork to keycloak
    
    * added hostNetwork under spec
    
    * minor syntax fix
    
    * minor syntax fix
    
    * fixed relative dir for k8s scripts
    
    * updated all manifests based on compose
    
    * small fix for yamls and script updates
    
    * added web and fixed configs
    
    * completed k8sdeploy, k8sremove scripts
    
    * changes to configuration
    
    * removed unnecessary file
    
    * fixes for running under docker desktop. keycloak issuer is problematic
    
    * fix oasapi probe
    
    * deployment script changes
    
    * removed portainer
    
    * removed network policies
    
    * full update to helm chart
    
    * config fixes to rootURL and INGRESSADDR
    
    * fixes to helm chart
    
    * made pv configs configurable from values
    
    * added README and values configs
    
    * minor fix to autoscaling input
    
    * added serviceName
    
    * added install script
    
    * removed ingress controller
    
    * added checkout to kubernetes branch
    
    * minor fix to rooturl
    
    * added routing for ingress
    
    * made update noninteractive
    
    * chmod +x install
    
    * temp remove needsrestart
    
    * minor fix to repo stage
    
    * repositioned needsrestart
    
    * completely removed needsrestart
    
    * added hostNetwork config
    
    * removed iptable routing
    
    * added hostNetwork to keycloak
    
    * added hostNetwork under spec
    
    * minor syntax fix
    
    * minor syntax fix
    
    * removed portainer
    
    ---------
    
    Co-authored-by: Christos Tranoris <tranoris@gmail.com>

--- kubernetes/k8sdeploy.sh
+++ kubernetes/k8sdeploy.sh
@@ -1,67 +1,65 @@
 #!/bin/bash	
-cp -r template/ deployment
+
+CURRENT_DIR="$(pwd)"
+SCRIPT_DIR="$(dirname "$0")"
+cd $SCRIPT_DIR
+
+# copy to a workingdir
+cp -r ./template ./_apply
 
 kubectl create namespace openslice
-kubectl apply -f ./deployment/openslice-ingress.yaml
+
+kubectl apply -f ./_apply/openslice-ingress.yaml
 
 # wait for ingress to get IP address
 INGRESSADDR=""
 while [ -z $INGRESSADDR ]; do
   echo "Waiting for external IP"
   INGRESSADDR=$(kubectl -n openslice get ingress openslice-ingress -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+  if [ -z "$INGRESSADDR" ]
+  then
+    echo "\INGRESSADDR is empty trying hostname"
+    INGRESSADDR=$(kubectl -n openslice get ingress openslice-ingress -o jsonpath='{.status.loadBalancer.ingress[0].hostname}')
+  else
+    echo "\$INGRESSADDR found!"
+  fi
   [ -z "$INGRESSADDR" ] && sleep 5
 done
 echo 'Found external IP: '$INGRESSADDR
+find ./_apply/ -type f  -print0 |  xargs -0 sed -i "s/INGRESSADDR/$INGRESSADDR/g"
 
-find ./deployment/ -type f  -print0 |  xargs -0 sed -i "s/INGRESSADDR/$INGRESSADDR/g"
+kubectl apply -f ./_apply/artemis.yaml
+kubectl apply -f ./_apply/bugzilla.yaml
+kubectl apply -f ./_apply/centrallog.yaml
+kubectl apply -f ./_apply/manoclient.yaml
+kubectl apply -f ./_apply/kroki.yaml
+kubectl apply -f ./_apply/blockdiag.yaml
 
-kubectl apply -f ./deployment/mysql-portal-claim0-persistentvolumeclaim.yaml
+kubectl apply -f ./_apply/portalweb-config.yaml
+kubectl apply -f ./_apply/portalweb-config-nginx.yaml
+kubectl apply -f ./_apply/portalweb.yaml
 
-kubectl apply -f ./deployment/activemq-deployment.yaml
-kubectl apply -f ./deployment/activemq-service.yaml
+kubectl apply -f ./_apply/tmfweb-config.yaml
+kubectl apply -f ./_apply/tmfweb-config-nginx.yaml
+kubectl apply -f ./_apply/tmfweb.yaml
 
-kubectl apply -f ./deployment/consul-deployment.yaml
-kubectl apply -f ./deployment/consul-service.yaml
+kubectl apply -f ./_apply/mysql-config.yaml
+kubectl apply -f ./_apply/mysql-pv-pvc.yaml
+kubectl apply -f ./_apply/mysql.yaml
 
-kubectl apply -f ./deployment/mysql-portal-deployment.yaml
-kubectl apply -f ./deployment/mysql-portal-service.yaml
+kubectl create configmap keycloak-realm-config --from-file=./_apply/realm-export.json --namespace=openslice
+kubectl apply -f ./_apply/keycloak.yaml
 
-#kubectl apply -f ./deployment/keycloak-realm-config.yaml
-kubectl create configmap keycloak-realm-config --from-file=./deployment/realm-export.json --namespace=openslice
-kubectl apply -f ./deployment/keycloak-deployment.yaml
-kubectl apply -f ./deployment/keycloak-service.yaml
+kubectl apply -f ./_apply/osom-pv-pvc.yaml
+kubectl apply -f ./_apply/osom.yaml
 
-kubectl apply -f ./deployment/osom-claim0-persistentvolumeclaim.yaml
-kubectl apply -f ./deployment/osom-deployment.yaml
-kubectl apply -f ./deployment/osom-service.yaml
+kubectl apply -f ./_apply/oasapi-pv-pvc.yaml
+kubectl apply -f ./_apply/oasapi.yaml
 
-kubectl apply -f ./deployment/manoclient-deployment.yaml
-kubectl apply -f ./deployment/manoclient-service.yaml
+kubectl apply -f ./_apply/osportalapi-pv-pvc.yaml
+kubectl apply -f ./_apply/osportalapi.yaml
 
-kubectl apply -f ./deployment/osportalapi-claim0-persistentvolumeclaim.yaml
-kubectl apply -f ./deployment/osportalapi-deployment.yaml
-kubectl apply -f ./deployment/osportalapi-service.yaml
+kubectl apply -f ./_apply/osscapi-pv-pvc.yaml
+kubectl apply -f ./_apply/osscapi.yaml
 
-kubectl apply -f ./deployment/osscapi-claim0-persistentvolumeclaim.yaml
-kubectl apply -f ./deployment/osscapi-deployment.yaml
-kubectl apply -f ./deployment/osscapi-service.yaml
-
-kubectl apply -f ./deployment/oasapi-claim0-persistentvolumeclaim.yaml
-kubectl apply -f ./deployment/oasapi-deployment.yaml
-kubectl apply -f ./deployment/oasapi-service.yaml
-
-kubectl apply -f ./deployment/tmfweb-config.yaml
-kubectl apply -f ./deployment/tmfweb-deployment.yaml
-kubectl apply -f ./deployment/tmfweb-service.yaml
-
-kubectl apply -f ./deployment/portalweb-config.yaml
-kubectl apply -f ./deployment/portalweb-deployment.yaml
-kubectl apply -f ./deployment/portalweb-service.yaml
-
-kubectl apply -f ./deployment/centrallog-deployment.yaml
-kubectl apply -f ./deployment/centrallog-service.yaml
-
-kubectl apply -f ./deployment/bugzilla-client-deployment.yaml
-kubectl apply -f ./deployment/bugzilla-client-service.yaml
-
-
+cd $CURRENT_DIR
